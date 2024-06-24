767fd2f4961f13abfa88b8086746fdfeb4b48445
laxmi013 <laxmiray013@gmail.com>
2024-06-24 23:33:05 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -18,9 +18,9 @@ const Chats = require('./Model/Schema');
 const PORT = 3000; 
 
 const app = express();
-    app.use(express.static(path.join(__dirname, '..', 'dist')));
+    app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
     app.get('*', (req, res) => {
-       res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+       res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
 
