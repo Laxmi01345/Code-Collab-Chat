0c29e828adf1207d5e96ba7cbd42b87689cd204b
Laxmi Ray <122423386+Laxmi01345@users.noreply.github.com>
2024-06-27 09:00:03 +0530

    Update Input.jsx

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -7,7 +7,7 @@ export const Input = ({setCustomInput}) => {
     };
     return (
         <>
-            <div className='text-xl w-80 md:w-96'>
+            <div className='text-xl w-96'>
                 <span>Input</span>
                 <textarea className="border-2 bg-black text-white  text-sm p-2
                 
